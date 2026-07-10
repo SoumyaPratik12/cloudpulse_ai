fbd5a946574b0dff54f6e42fd9447adff1be55f1
SoumyaPratik12 <61589208+SoumyaPratik12@users.noreply.github.com>
2026-07-10 13:33:32 +0530

    feat: add infrastructure/variables.tf configurations

---
+++ infrastructure/variables.tf
@@ -0,0 +1,42 @@
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS Region to deploy resources"
+}
+
+variable "environment" {
+  type        = string
+  default     = "prod"
+  description = "Deployment environment (dev, prod)"
+}
+
+variable "app_name" {
+  type        = string
+  default     = "cloudpulse"
+  description = "Application name prefix"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "VPC CIDR block"
+}
+
+variable "db_password" {
+  type        = string
+  sensitive   = true
+  default     = "CloudPulseSuperSecurePassword123!"
+  description = "Password for PostgreSQL database"
+}
+
+variable "backend_image" {
+  type        = string
+  default     = "ghcr.io/soumyapratik12/cloudpulse_ai-backend:latest"
+  description = "FastAPI backend Docker image tag"
+}
+
+variable "frontend_image" {
+  type        = string
+  default     = "ghcr.io/soumyapratik12/cloudpulse_ai-frontend:latest"
+  description = "React frontend Docker image tag"
+}
