6bd52ff8fea9576d282879dd5b4e2ac294d9be2d
SoumyaPratik12 <61589208+SoumyaPratik12@users.noreply.github.com>
2026-07-10 13:33:58 +0530

    feat: add infrastructure/rds.tf configurations

---
+++ infrastructure/rds.tf
@@ -0,0 +1,41 @@
+resource "aws_db_subnet_group" "db_subnets" {
+  name       = "${var.app_name}-${var.environment}-db-subnets"
+  subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+  tags = {
+    Name = "${var.app_name}-${var.environment}-db-subnet-group"
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "${var.app_name}-${var.environment}-rds-sg"
+  description = "Allow inbound PostgreSQL traffic from ECS Fargate Tasks"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs_tasks.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "db" {
+  identifier             = "${var.app_name}-${var.environment}-db"
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "15.4"
+  instance_class         = "db.t3.micro"
+  db_name                = "cloudpulse"
+  username               = "postgres"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.db_subnets.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot    = true
+}
