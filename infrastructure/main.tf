e42e155c0fe3bf8c1e922a5c268005c66e34c0a6
SoumyaPratik12 <sethisoumya@gmail.com>
2026-07-14 14:03:42 +0530

    feat: configure remote S3 backend for persistent Terraform state

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">= 1.5.0"
+  backend "s3" {
+    bucket         = "cloudpulse-tf-state-748189524661"
+    key            = "production/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "cloudpulse-tf-locks"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
